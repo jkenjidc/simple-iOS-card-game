fdeddbe553c0c0726dd3d004427b4a0bf8a8adaf
kenji dela cruz <kenji.delacruz@willowtreeapps.com>
2022-06-29 15:08:01 -0400

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Justine Kenji Dela Cruz on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
